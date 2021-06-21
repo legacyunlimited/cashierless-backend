e159b96b1dd8a1e17da51b7005e98960effa706a
legacyunlimited <71147160+legacyunlimited@users.noreply.github.com>
2021-06-21 12:31:22 +0530

    milestone 2

---
+++ app.js
@@ -0,0 +1,55 @@
+require("dotenv").config();
+
+const mongoose = require("mongoose");
+const express = require("express");
+const app = express();
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+var { isUnauthorized } = require("./middlewares/auth");
+
+//Routes
+const authRoutes = require("./routes/auth");
+const userRoutes = require("./routes/user");
+const categoryRoutes = require("./routes/category");
+const productRoutes = require("./routes/product");
+const orderRoutes = require("./routes/order");
+const stripeRoutes = require("./routes/stripe");
+
+//DB Connection
+mongoose
+  .connect(process.env.DATABASE_TEST, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => {
+    console.log("DB CONNECTED");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+//Middlewares
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
+
+//Routes
+app.use("/api", authRoutes);
+app.use("/api", userRoutes);
+app.use("/api", categoryRoutes);
+app.use("/api", productRoutes);
+app.use("/api", orderRoutes);
+app.use("/api", stripeRoutes);
+
+//Custom middleware for error handling in express-jwt
+app.use(isUnauthorized);
+
+//PORT
+const port = process.env.PORT || 8000;
+
+//Starting a server
+app.listen(port, () => {
+  console.log(`app is running at ${port}`);
+});
---
+++ routes/order.js
@@ -0,0 +1,58 @@
+var express = require("express");
+var router = express.Router();
+const {
+  isSignedin,
+  isAuthenticated,
+  isCustomer,
+  isGuard,
+  isManager,
+} = require("../middlewares/auth");
+const { updateStock } = require("../middlewares/product");
+const { getUserById, pushOrderInPurchaseList } = require("../middlewares/user");
+const {
+  createOrder,
+  getAllOrders,
+  getOrderStatus,
+  updateStatus,
+  getOrderDetails,
+} = require("../controllers/order");
+const { getOrderById } = require("../middlewares/order");
+
+router.param("userId", getUserById);
+router.param("orderId", getOrderById);
+
+// Get all orders
+router.get(
+  "/order/all/:userId",
+  isSignedin,
+  isAuthenticated,
+  isManager,
+  getAllOrders
+);
+
+// Get all status in enums
+router.get(
+  "/order/status/:userId",
+  isSignedin,
+  isAuthenticated,
+  isManager,
+  getOrderStatus
+);
+
+// Get order details
+router.get("/order/:orderId", getOrderDetails);
+
+// Admin can change status of orders
+router.put("/order/status", isSignedin, updateStatus);
+
+//create a new order createOrder
+router.post(
+  "/order/create/:userId",
+  isSignedin,
+  isAuthenticated,
+  isCustomer,
+  pushOrderInPurchaseList,
+  createOrder
+);
+
+module.exports = router;
